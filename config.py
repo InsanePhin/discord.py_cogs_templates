a6d3817d433fe0486b509c4d151cfeace7fd98e6
Minseo Seo <insanephin@gmail.com>
2021-06-09 08:33:02 +0900

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-Bot={
-    "token": "NjgyMjI1NjQ0MTM4MDA0NTQ5.XlZ6JA.C5tyX8JDocxJusYSTf3NnSnZxuQ",
-    "default_prefix": "!"
-},
-Mysql={
-    "id": "nature",
-    "password": "bmF0dXJlDQo=",
-    "db": "naturebot"
-}
