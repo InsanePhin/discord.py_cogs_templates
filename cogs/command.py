3823cf8534638cfca2f5db1f34dc88d2b57bf999
Minseo Seo <insanephin@gmail.com>
2021-08-10 11:18:23 +0900

    Update command.py

--- cogs/command.py
+++ cogs/command.py
@@ -1,13 +1,10 @@
 #-*- coding:utf-8 -*-
 
-import discord, asyncio, requests, os
 from discord.ext import commands
 
 class Command(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-
 
 def setup(bot):
     bot.add_cog(Command(bot))
