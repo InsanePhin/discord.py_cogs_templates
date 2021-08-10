1a749a81c47e8f58bd405f6ff50cb249c9c39bd4
Minseo Seo <insanephin@gmail.com>
2021-08-10 11:31:03 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 #-*- coding:utf-8 -*-
 
-import discord, os
+import discord, os, jishaku
 from discord.ext import commands
 
 TOKEN=""    # YOUR TOKEN HERE
@@ -8,39 +8,12 @@ PREFIX=""   # BOT PREFIX HERE
 
 bot = commands.Bot(help_command=None, command_prefix=commands.when_mentioned_or(PREFIX), intents=discord.Intents.all())
 
+bot.load_extension("Jishaku")
 for filename in os.listdir('cogs'):
     if filename.endswith('.py'):
         try:
             bot.load_extension("cogs." + filename[:-3])
         except Exception as e:
             print(f'failed loaded. file : {filename} error : {e}')
-
-@bot.command(aliases=['reload', 'rl'])
-@commands.is_owner()
-async def _reload_all(ctx):
-    error_collection = []
-    for file in os.listdir("cogs"):
-        if file.endswith(".py"):
-            name = file[:-3]
-            try:
-                bot.reload_extension(f"cogs.{name}")
-            except Exception as e:
-                error_collection.append(
-                    [file, e]
-                )
-
-    if error_collection:
-        output = "\n".join(
-            [f"**{g[0]}**" for g in error_collection]   
-        )
-        return await ctx.send(
-            f"Check the {output} file"
-        )
-
-    return await ctx.message.reply(f"reloaded `{len(bot.commands)}` commands.")
-
-@_reload_all.error
-async def reloadall_handler(ctx, error):
-    pass
 
 bot.run(TOKEN)
